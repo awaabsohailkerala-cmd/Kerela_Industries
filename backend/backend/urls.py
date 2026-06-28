cf68a7ccd2593c518954b921b4d957bad4ed1a36
Uzair-Waseem-390 <uzairwaseem390@gmail.com>
2026-06-28 16:18:08 +0500

    fixing imports in urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from users.urls import auth_urlpatterns, user_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
